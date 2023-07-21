86b53d83fdd3d694ddaf80feff3e3c6166766464
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-07-21 03:21:06 +0100

    ML bugging

--- ChatBotTutor/chatbot/urls.py
+++ ChatBotTutor/chatbot/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.initialize, name='initialize'),
     path('get_text/<str:message>', views.get_text, name='get_text'),
+    path('index', views.index, name='index')
 ]
